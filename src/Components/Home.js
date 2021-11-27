7f9280f4344fa93f0d041cb78bc1e49e609f0f07
afaqshaikh <zaidsmai920@hotmail.com>
2021-11-27 11:24:19 +0500

    Add home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,21 +1,36 @@
 import { useDispatch, useSelector } from 'react-redux';
 import Header from "./Header";
 import Footer from "./Footer";
+import { Link } from "react-router-dom"
 
 const Home = () => {
     const diaries = useSelector((state) => state.diaries);
     const dispatch = useDispatch()
-        console.log(diaries)
+    console.log(diaries)
 
-    
-  
+
+
 
 
 
     return (
         <div className="">
             <Header />
-
+            <div className="container">
+                <div className="row mb-4 p-0">
+                    <div className="text-end p-0">
+                        <Link className="btn btn-success">Create Diary</Link>
+                    </div>
+                </div>
+                <div className="row">
+                    <textarea className="form-control rounded-0 rounded-top" placeholder="Leave a comment here" style={{ height: '100px' }}>
+                        Hello my name is babun!!!
+                    </textarea>
+                </div>
+                <div className="row bg-dark rounded-bottom text-end">
+                    <p className="text-white mt-2">Time</p>
+                </div>
+            </div>
             <Footer />
         </div>
     )
